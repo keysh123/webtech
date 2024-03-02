96e41b0992137ac7c4387aa2b95dec203f3a27a7
keysh123 <keyashah126@gmail.com>
2024-03-02 16:10:50 +0530

    dgd

--- backendGGA/app.js
+++ backendGGA/app.js
@@ -16,7 +16,7 @@ if(process.env.NODE_ENV!="PRODUCTION"){
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000','https://shopeeeewebtech.netlify.app/'],
+    origin: ['http://localhost:3000','https://shopeeeewebtech.netlify.app'],
   })
 );
 app.use(express.json());
